b43c89b89068f46a9f5ee202a82402fa9608f0af
Yasin Silavi <ys.silavi@gmail.com>
2024-01-17 00:03:18 +0330

    feat: add some color and spacing variables

---
+++ configs/theme/variables.stylex.ts
@@ -0,0 +1,9 @@
+import * as stylex from '@stylexjs/stylex'
+
+export const colors = stylex.defineVars({
+  black: '#1d1c1c',
+  gray: '#313d44',
+  gray1: '#cccbc8',
+})
+
+export const spacing = stylex.defineVars({})
