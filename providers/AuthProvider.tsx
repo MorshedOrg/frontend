2eb8d462e840abf720d21c6f2382326dfee96140
Yasin Silavi <ys.silavi@gmail.com>
2024-02-07 19:40:16 +0330

    feat: add access-token and refresh-token states

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -1,23 +1,35 @@
 'use client'
 
-import { Dispatch, ReactNode, SetStateAction, createContext, useContext, useMemo, useState } from 'react'
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useContext,
+  useMemo,
+  useState,
+} from 'react'
 
 interface AuthContextValues {
-  phone?: string
-  otpSession?: string
-  setPhone?: Dispatch<SetStateAction<string>>
-  setOtpSession?: Dispatch<SetStateAction<string>>
+  phone: string
+  otpSession: string
+  accessToken: string
+  refreshToken: string
+  setPhone: Dispatch<SetStateAction<string>>
+  setOtpSession: Dispatch<SetStateAction<string>>
+  setAccessToken: Dispatch<SetStateAction<string>>
+  setRefreshToken: Dispatch<SetStateAction<string>>
 }
 
-const AuthContext = createContext<AuthContextValues>({})
+const AuthContext = createContext<AuthContextValues | undefined>(undefined)
 
 export function useAuthContext() {
   const context = useContext(AuthContext)
-  
+
   if (context === undefined) {
     throw new Error('useAuthContext was used outside of its Provider')
   }
-  
+
   return context
 }
 
@@ -28,15 +40,21 @@ type AuthProviderProps = {
 export default function AuthProvider({ children }: AuthProviderProps) {
   const [phone, setPhone] = useState('')
   const [otpSession, setOtpSession] = useState('')
+  const [accessToken, setAccessToken] = useState('')
+  const [refreshToken, setRefreshToken] = useState('')
 
   const values = useMemo(
     () => ({
       phone,
       setPhone,
       otpSession,
+      accessToken,
+      refreshToken,
       setOtpSession,
+      setAccessToken,
+      setRefreshToken,
     }),
-    [otpSession, phone]
+    [accessToken, otpSession, phone, refreshToken]
   )
 
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
